bb76034240fb2ab4c2e1779d7c8c7cd3a2a5a0fc
hikaru-bit <mracademix@gmail.com>
2021-02-01 08:59:54 +0000

    start

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'homes/about' => 'homes#about'
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+
+end
